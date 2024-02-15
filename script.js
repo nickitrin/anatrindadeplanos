59469b9970d6196ed8639d8b7ac52d8d5d999d9f
Nicole Trindade <nicoletrindade72@gmail.com>
2024-02-15 13:15:40 -0300

    style flow animation

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop >= 0 || document.documentElement.scrollTop >= 0) {
-        document.getElementById("myBtn").style.display = "block";
-    } 
-}
+function toggleMenu() {
+    const menusSuperiores = document.querySelectorAll('.menu_superior');
+  
+    menusSuperiores.forEach(menuSuperior => {
+      // Verifica se o menu superior está visível
+      if (menuSuperior.style.display === 'none' || menuSuperior.style.display === '') {
+        // Se estiver oculto ou sem estilo, exibe-o
+        menuSuperior.style.display = 'flex';
+      } else {
+        // Caso contrário, esconde-o
+        menuSuperior.style.display = 'none';
+      }
+    });
+  }
