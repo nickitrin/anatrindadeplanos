11ee53ea2b88e01274173b6efb2fb12af68beabc
Nicole Trindade <nicoletrindade72@gmail.com>
2024-02-15 13:24:14 -0300

    melhorias menu

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function toggleMenu() {
-    const menusSuperiores = document.querySelectorAll('.menu_superior');
+    if (window.innerWidth <= 767) { // Verifica a largura da tela
+      const menusSuperiores = document.querySelectorAll('.menu_superior');
   
-    menusSuperiores.forEach(menuSuperior => {
-      // Verifica se o menu superior está visível
-      if (menuSuperior.style.display === 'none' || menuSuperior.style.display === '') {
-        // Se estiver oculto ou sem estilo, exibe-o
-        menuSuperior.style.display = 'flex';
-      } else {
-        // Caso contrário, esconde-o
-        menuSuperior.style.display = 'none';
-      }
-    });
+      menusSuperiores.forEach(menuSuperior => {
+        // Verifica se o menu superior está visível
+        if (menuSuperior.style.display === 'none' || menuSuperior.style.display === '') {
+          // Se estiver oculto ou sem estilo, exibe-o
+          menuSuperior.style.display = 'flex';
+        } else {
+          // Caso contrário, esconde-o
+          menuSuperior.style.display = 'none';
+        }
+      });
+    }
   }
+  
